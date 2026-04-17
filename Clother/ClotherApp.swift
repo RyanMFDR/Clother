7dd2ac512480dcdefcd996ff71ea0d6ced68fc7c
Ryan <ryanmfdr@gmail.com>
2026-04-17 16:25:03 +0800

    Initial Commit

---
+++ Clother/ClotherApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClotherApp.swift
+//  Clother
+//
+//  Created by RyanMFDR on 17/04/26.
+//
+
+import SwiftUI
+
+@main
+struct ClotherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
