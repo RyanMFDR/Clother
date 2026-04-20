cd256e66f588c7fff32a66452688400a6b5489df
Luisa <luisahaning.t@gmail.com>
2026-04-20 15:06:33 +0800

    incomplete first screen sturcture

--- Clother/ContentView.swift
+++ Clother/ContentView.swift
@@ -9,13 +9,58 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        
+        ZStack{
+            //background
+            LinearGradient(colors: [.orange, .yellow, .white], startPoint: .top, endPoint: .bottom)
+                .ignoresSafeArea(edges: .all)
+            
+            //start scroll view
+            ScrollView(.vertical, showsIndicators: false){
+                Spacer()
+                HStack{
+                    Image(systemName: "location.fill")
+                    Text("Kabupaten Badung")
+                }
+                ZStack{
+                    Image(systemName: "figure.arms.open")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 400, height: 400)
+                        .foregroundStyle(Color.white)
+                    VStack{
+                        Spacer()
+                        HStack{
+                            Image(systemName: "chevron.left")
+                            Spacer()
+                            Image(systemName: "tshirt.fill")
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: 200, height: 200)
+                                .foregroundStyle(Color.blue)
+                                
+                            Spacer()
+                            Image(systemName: "chevron.right")
+                        }.padding(10)
+                        HStack{
+                            Image(systemName: "chevron.left")
+                            Spacer()
+                            Image(systemName: "square.fill")
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: 100, height: 100)
+                                .foregroundStyle(Color.blue)
+                                
+                            Spacer()
+                            Image(systemName: "chevron.right")
+                        }.padding(10)
+                    }
+                    
+                }
+                
+                
+            }
         }
-        .padding()
     }
 }
 
